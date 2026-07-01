8691f1d46ced32e8dc8aa25a25f1cb5d0964fac9
vaguesdelune <dupont.alexandre@icloud.com>
2026-07-01 13:24:03 -0400

    fix scroll path

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ window.addEventListener('wheel', (event) => {
 
   if (currentStep >= targetStep && !hasLoaded) {
     hasLoaded = true;
-    window.location.href = 'https://youtube.com';
+    window.location.href = 'library/';
   }
 }, { passive: false });
 
